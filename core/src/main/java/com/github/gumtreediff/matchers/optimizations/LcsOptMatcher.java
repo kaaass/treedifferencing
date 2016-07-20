943b0d5a1d803f92601c9e7318cb9b411c2c9b73
narmion <Georg.Dotzler@fau.de>
2016-07-20 21:23:38 +0200

    Updated to ASE paper version.

--- core/src/main/java/com/github/gumtreediff/matchers/optimizations/LcsOptMatcher.java
+++ core/src/main/java/com/github/gumtreediff/matchers/optimizations/LcsOptMatcher.java
@@ -1,24 +1,27 @@
 /*
  * This file is part of GumTree.
  *
- * GumTree is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * GumTree is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
  *
- * GumTree is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * GumTree is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTIcULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with GumTree.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License along with GumTree. If
+ * not, see <http://www.gnu.org/licenses/>.
  *
- * Copyright 2015 Georg Dotzler <georg.dotzler@fau.de>
- * Copyright 2015 Marius Kamp <marius.kamp@fau.de>
+ * Copyright 2015 Georg Dotzler <georg.dotzler@fau.de> Copyright 2015 Marius Kamp
+ * <marius.kamp@fau.de>
  */
 
 package com.github.gumtreediff.matchers.optimizations;
+
+import com.github.gumtreediff.matchers.Mapping;
+import com.github.gumtreediff.matchers.MappingStore;
+import com.github.gumtreediff.matchers.Matcher;
+import com.github.gumtreediff.tree.ITree;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -26,25 +29,26 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
-import com.github.gumtreediff.matchers.Mapping;
-import com.github.gumtreediff.matchers.MappingStore;
-import com.github.gumtreediff.matchers.Matcher;
-import com.github.gumtreediff.tree.ITree;
-
 /**
- * 
- * This implements lcs optimization Theta B.
+ * This implements the lcs optimization Theta B.
  *
  */
 
-public class LCSOptMatcher extends Matcher {
+public class LcsOptMatcher extends Matcher {
 
-	public LCSOptMatcher(ITree src, ITree dst, MappingStore store) {
+	/**
+	 * Instantiates a new lcs matcher.
+	 *
+	 * @param src the src
+	 * @param dst the dst
+	 * @param store the store
+	 */
+	public LcsOptMatcher(ITree src, ITree dst, MappingStore store) {
 		super(src, dst, store);
 
 	}
 
-	private void advancedLCSMatching() {
+	private void advancedLcsMatching() {
 		List<ITree> allNodesSrc = src.getTrees();
 		List<ITree> allNodesDst = dst.getTrees();
 		Set<ITree> unmatchedNodes1 = new HashSet<>();
@@ -106,20 +110,23 @@ public class LCSOptMatcher extends Matcher {
 		}
 	}
 
-	private void backtrack(ArrayList<ITree> list1, ArrayList<ITree> list2, LinkedList<Mapping> resultList,
-			int[][] matrix, int i, int j, Set<ITree> unmatchedNodes1, Set<ITree> unmatchedNodes2) {
-		assert (i >= 0);
-		assert (j >= 0);
-		while (i > 0 && j > 0) {
-			if (testCondition(list1.get(i - 1), list2.get(j - 1), unmatchedNodes1, unmatchedNodes2)) {
-				if (!mappings.hasSrc(list1.get(i - 1))) {
-					resultList.add(new Mapping(list1.get(i - 1), list2.get(j - 1)));
+	private void backtrack(ArrayList<ITree> list1, ArrayList<ITree> list2,
+						   LinkedList<Mapping> resultList, int[][] matrix, int ipar, int jpar,
+						   Set<ITree> unmatchedNodes1,
+						   Set<ITree> unmatchedNodes2) {
+		assert (ipar >= 0);
+		assert (jpar >= 0);
+		while (ipar > 0 && jpar > 0) {
+			if (testCondition(list1.get(ipar - 1), list2.get(jpar - 1), unmatchedNodes1,
+					unmatchedNodes2)) {
+				if (!mappings.hasSrc(list1.get(ipar - 1))) {
+					resultList.add(new Mapping(list1.get(ipar - 1), list2.get(jpar - 1)));
 				}
 			}
-			if (matrix[i][j - 1] > matrix[i - 1][j]) {
-				j--;
+			if (matrix[ipar][jpar - 1] > matrix[ipar - 1][jpar]) {
+				jpar--;
 			} else {
-				i--;
+				ipar--;
 			}
 		}
 	}
@@ -144,8 +151,8 @@ public class LCSOptMatcher extends Matcher {
 		}
 	}
 
-	public List<Mapping> lcs(ArrayList<ITree> list1, ArrayList<ITree> list2, Set<ITree> unmatchedNodes1,
-			Set<ITree> unmatchedNodes2) {
+	private List<Mapping> lcs(ArrayList<ITree> list1, ArrayList<ITree> list2,
+							  Set<ITree> unmatchedNodes1, Set<ITree> unmatchedNodes2) {
 		int[][] matrix = new int[list1.size() + 1][list2.size() + 1];
 		for (int i = 1; i < list1.size() + 1; i++) {
 			for (int j = 1; j < list2.size() + 1; j++) {
@@ -157,17 +164,32 @@ public class LCSOptMatcher extends Matcher {
 			}
 		}
 		LinkedList<Mapping> resultList = new LinkedList<>();
-		backtrack(list1, list2, resultList, matrix, list1.size(), list2.size(), unmatchedNodes1, unmatchedNodes2);
+		backtrack(list1, list2, resultList, matrix, list1.size(), list2.size(), unmatchedNodes1,
+				unmatchedNodes2);
 		return resultList;
 	}
 
+
+	/**
+	 * Match with Theta B.
+	 */
 	@Override
 	public void match() {
-		advancedLCSMatching();
+		advancedLcsMatching();
 
 	}
 
-	public boolean testCondition(ITree node1, ITree node2, Set<ITree> unmatchedNodes1, Set<ITree> unmatchedNodes2) {
+	/**
+	 * Compare two nodes to test lcs condition.
+	 *
+	 * @param node1 the node1
+	 * @param node2 the node2
+	 * @param unmatchedNodes1 the unmatched nodes1
+	 * @param unmatchedNodes2 the unmatched nodes2
+	 * @return true, if successful
+	 */
+	public boolean testCondition(ITree node1, ITree node2, Set<ITree> unmatchedNodes1,
+								 Set<ITree> unmatchedNodes2) {
 		if (node1.getType() != node2.getType()) {
 			return false;
 		}
